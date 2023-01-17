8d09629c5c9b582bbbe136e53b8398460d6d090c
mayconline <mayconline.ti@gmail.com>
2023-01-17 17:37:20 -0300

    add history page

---
+++ mobile/src/screens/History.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import HistoryLogo from '../../assets/svg/HistoryLogo';
+
+export function History() {
+  return (
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 justify-center items-center px-3 bg-gray-100 dark:bg-gray-900">
+        <HistoryLogo className="h-56 w-64" />
+
+        <Text className="text-gray-500 dark:text-gray-100 font-bold text-2xl">
+          Em breve
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
